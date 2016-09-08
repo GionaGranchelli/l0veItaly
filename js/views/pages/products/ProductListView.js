015e3ea54cd1db41aae6ea785e418a57936e2e67
GionaGranchelli <gionagranchelli@gmail.com>
2016-09-08 13:29:56 +0200

    partial infinte scroll

--- js/views/pages/products/ProductListView.js
+++ js/views/pages/products/ProductListView.js
@@ -1,18 +1,19 @@
 define(function(require) {
 
   var Backbone = require("backbone");
-//  var MyModel = require("models/MyModel");
+  //  var MyModel = require("models/MyModel");
   var Utils = require("utils");
   var ProductDetailView = require('views/pages/products/ProductDetailView');
   var Products = require("collections/Products");
-  
+
   var ProductListView = Utils.Page.extend({
 
     constructorName: "ProductListView",
     collection: Products,
 
     initialize: function() {
-        
+
+      _.bindAll(this, 'render');
       // load the precompiled template
       this.template = Utils.templates.productlist;
       this.collection.on('sync', this.render, this);
@@ -32,12 +33,13 @@ define(function(require) {
 
     events: {
       "tap #goToMap": "goToMap",
-      "tap #goToProductDetail" : "goToProductDetail"
+      "tap #goToProductDetail" : "goToProductDetail",
+      "scroll" : "fetchSheets"
     },
 
     render: function() {
       $(this.el).html(this.template({Prodotti: this.collection.toJSON()}));
-//      this.model.toJSON()
+      //      this.model.toJSON()
       return this;
     },
 
@@ -47,14 +49,36 @@ define(function(require) {
       });
     },
     goToProductDetail: function(ev){
-        
-        Backbone.history.navigate("gotoproductdetail/" + $(ev.currentTarget).data('id'), {
+
+      Backbone.history.navigate("gotoproductdetail/" + $(ev.currentTarget).data('id'), {
         trigger: true
       });
-      
+
+    },
+    fetchSheets: function () {
+
+      var delta = this.checkScroll();
+      console.log(delta);
+      if (delta > -20) {
+        console.log('this.collection =>' + this.collection);
+        console.log('this.collection.initiale = >' + this.collection.initiale);
+        console.log('this.collection.limite =>' + this.collection.limite);
+        this.collection.initiale += 5;
+        this.collection.limite += 5;
+        this.collection.fetch();
+      }
+    },
+    checkScroll: function () {
+      var scrollHeight = this.el.offsetHeight;
+                //  console.log("scrollHeight " + scrollHeight);
+      var scrollTop = this.el.scrollHeight;// Altezza del contenuto di Page
+                //  console.log("scrollTop " + scrollTop);
+      var offsetHeight = this.el.scrollTop;  // Delta spostamento dello spostamento
+                //  console.log("offsetHeight" + offsetHeight);
+      return (scrollHeight - (scrollTop - offsetHeight));
     }
   });
 
-  return ProductListView;
+          return ProductListView;
 
 });
