870f2f9588efbb7480d9ebb7f0b9b84688718ada
Joe Grankellosky <giona.granchelli@gmail.com>
2016-09-09 12:17:14 +0200

    no message

--- js/collections/Products.js
+++ js/collections/Products.js
@@ -4,30 +4,50 @@ define(function (require) {
     var Product = require("models/Product");
     var Products = Backbone.Collection.extend({
         initialize: function (option, category, farm) {
-            
-            
-            console.log('option' + option);
-            console.log('category' + category);
-            console.log('typeof category' + typeof category);
-            console.log('farm' + farm);
-            
-            if (typeof option === "number") {
-                this.url += '&limit=' + option;
-            }
-            if (category !== undefined ) {
-                if (typeof category !== "string") {
-                    console.log("Products filtered by Category");
-                    this.url += '&filter[id_category_default]=[' + category + ']';
-                }
-            }
-            if (farm !== undefined) {
-                console.log("Products filtered by Farm");
-                this.url += '&filter[id_manufacturer]=[' + farm + ']';
-            }
+            this.initiale = 0;
+            this.limite = option;
+            this.category = category;
+            this.farm = farm;
+
+
         },
         model: Product,
-        url: 'http://loveitaly.altervista.org/api/products/?display=full&io_format=JSON&ws_key=IYI6M35MLB8UVW38Y99RY3YPQWRX5X8H',
+        initiale: 0,
+        limite : 4,
+        category : undefined,
+        farm : undefined,
+        urlRoot: 'http://loveitaly.altervista.org/api/products/?display=full&io_format=JSON&ws_key=IYI6M35MLB8UVW38Y99RY3YPQWRX5X8H',
+        url : function(){
+          console.log('option-> ' + this.option);
+          console.log('typeof option-> ' + typeof this.option);
+          console.log('category->' + this.category);
+          console.log('typeof category-> ' + typeof this.category);
+          console.log('farm->' + this.farm);
+          console.log('initial ->' + this.initiale);
+          console.log('limit ->' + this.limite);
+          var base = this.urlRoot;
+          if (typeof this.limite === "number") {
+              if( this.initiale === 0 ){
+                  base += '&limit=' + encodeURIComponent(this.limite);
+              }else{
+                  base += '&limit=' +encodeURIComponent(this.initiale) + ',' + encodeURIComponent(this.limite);
+              }
+
+          }
+          if (this.category !== undefined ) {
+              if (typeof category !== "string") {
+                  console.log("Products filtered by Category");
+                  base += '&filter[id_category_default]=[' + this.category + ']';
+              }
+          }
+          if (this.farm !== undefined) {
+              console.log("Products filtered by Farm");
+              base += '&filter[id_manufacturer]=[' + this.farm + ']';
+          }
+          return base;
+        },
         parse: function (data) {
+          console.log(this.base);
             return data.products;
         }
     });
