fbd4767390d1139a4fabb6510c69edebf487a001
GionaGranchelli <giona.granchelli@gmail.com>
2016-10-12 16:59:18 +0200

    congig

---
+++ js/views/pages/private/UpdateProfileView.js
@@ -0,0 +1,35 @@
+define(function(require) {
+
+  var Backbone = require("backbone");
+  var Utils = require("utils");
+  var Addresses = require("models/Addresses");
+  var ProfileView = Utils.Page.extend({
+    constructorName: "ProfileView",
+    addresses : Addresses,
+    initialize: function() {
+      // load the precompiled template
+      this.template = Utils.templates.profile;
+      this.addresses = new Addresses();
+      this.addresses.addIdAddress(window.customer.id);
+      this.addresses.fetch();
+      this.addresses.on('sync', this.render, this);
+      $('#back-button').css('display','block');
+      $('#toggle-button').css('display','none');
+    },
+    id: "profileview",
+    className: "i-g page",
+    events: {
+      
+    },
+    render: function() {
+      
+      $(this.el).html(this.template({
+          Customer : window.customer,
+          Addresses : this.addresses.toJSON()}));
+      return this;
+    }
+});
+
+  return ProfileView;
+
+});
