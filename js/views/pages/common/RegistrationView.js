1f0853b42536b849eec4b0573339cbc34f0d92a5
GionaGranchelli <giona.granchelli@gmail.com>
2016-10-30 17:21:59 +0100

    Sistemato grafica e Messaggio conferma Checkout

--- js/views/pages/common/RegistrationView.js
+++ js/views/pages/common/RegistrationView.js
@@ -1,5 +1,5 @@
 define(function (require) {
-    var validate = require("validate");
+//    var validate = require("validate");
     var Backbone = require("backbone");
     var Utils = require("utils");
     var Addresses = require("models/Addresses");
@@ -42,78 +42,43 @@ define(function (require) {
                 phone: $("#phone").val(),
                 phone_mobile: $("#phone_mobile").val()
             };
-             window.customer = data;
-             window.customer.logged = true;
-             window.localStorage.setItem('customer', JSON.stringify(window.customer));
-             window.localStorage.setItem('flag', JSON.stringify(true));
+            this.validate(data);
+            window.customer = data;
+            window.customer.logged = true;
+            window.localStorage.setItem('customer', JSON.stringify(window.customer));
+            window.localStorage.setItem('flag', JSON.stringify(true));
             console.log(window.customer);
-           window.location.href = "";
+            window.location.href = "";
 
         },
-        defaults: {
-            // valori di controllo
-            firstname: 'name-unknown',
-            lastname: 'surname-unknown',
-            email: 'user@example.com',
-            passwd: 'no-one',
-            active: 1, // si suppone che l'utente sia attivo
-            id_default_group: 3 // è un customer
+        validate: function (data) {
+            if(data.firstname.lenght > this.validationparameters.firstname.length.maximum || data.firstname === "" || data.firstname == null){
+                console.log("firstanme male");
+            }
         },
-        validationparameters:  {
+        validationparameters: {
             firstname: {
                 length: {
                     maximum: 32,
-                    tooLong : 'Massimo 32 caratteri'
-                },
-                format: {
-                    pattern: /^[^0-9!<>,;?=+()@#"°{}_$%:]*$/u,
-                    message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-                        return 'firstname-format-error'
-                    }
+                    tooLong: 'Massimo 32 caratteri'
                 }
             },
             lastname: {
                 length: {
                     maximum: 32,
                     tooLong: 'Massimo 32 caratteri'
-                },
-                format: {
-                    pattern: /^[^0-9!<>,;?=+()@#"°{}_$%:]*$/u,
-                    message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-                        return 'lastname-format-error'
-                    }
                 }
             },
             passwd: {
                 length: {
                     maximum: 32,
                     tooLong: 'Massimo 32 caratteri'
-                },
-                format: {
-                    pattern: /^[.a-zA-Z_0-9-!@#$%\^&*()]{5,32}$/,
-                    message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-                        return 'passwd-format-error'
-                    }
                 }
             },
             email: {
                 length: {
                     maximum: 128,
                     tooLong: 'Massimo 128 caratteri'
-                },
-                format: {
-                    pattern: /^[a-z0-9_-]+[.a-z0-9_-]*@[a-z0-9]+[._a-z0-9-]*\.[a-z0-9]+$/ui,
-                    message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-                        return 'email-format-error'
-                    }
-                }
-            },
-            birthday: {
-                format: {
-                    pattern: /^([0-9]{4})-((0?[1-9])|(1[0-2]))-((0?[1-9])|([1-2][0-9])|(3[01]))( [0-9]{2}:[0-9]{2}:[0-9]{2})?$/,
-                    message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-                        return 'email-format-error'
-                    }
                 }
             }
         }
