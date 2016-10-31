2ebbdf484586ef081b4dd565dbff748a85520524
GionaGranchelli <giona.granchelli@gmail.com>
2016-10-31 09:38:46 +0100

    validation Update e Registration

--- js/views/pages/private/UpdateProfileView.js
+++ js/views/pages/private/UpdateProfileView.js
@@ -26,16 +26,6 @@ define(function (require) {
             return this;
         },
         runUpdate: function (ev) {
-            navigator.notification.confirm(
-                    'Sei Sicuro delle Modifiche?', // message
-                    this.onConfirm, // callback to invoke with index of button pressed
-                    'Modifiche', // title
-                    ['Procedi', 'No!!']     // buttonLabels
-                    );
-
-        },
-        onConfirm: function (buttonIndex) {
-
             var data = {
                 id: $("#idCostumer").val(),
                 firstname: $("#firstname").val(),
@@ -49,6 +39,34 @@ define(function (require) {
                 phone: $("#phone").val(),
                 phone_mobile: $("#phone_mobile").val()
             };
+
+            if (this.validate(data)) {
+                navigator.notification.confirm(
+                        'Sei Sicuro delle Modifiche?', // message
+                        this.onConfirm, // callback to invoke with index of button pressed
+                        'Modifiche', // title
+                        ['Procedi', 'No!!']     // buttonLabels
+                        );
+            }
+
+
+
+        },
+        onConfirm: function (buttonIndex) {
+            var data = {
+                id: $("#idCostumer").val(),
+                firstname: $("#firstname").val(),
+                lastname: $("#lastname").val(),
+                city: $("#city").val(),
+                birthday: $("#birthday").val(),
+                address1: $("#address1").val(),
+                address2: $("#address2").val(),
+                email: $("#email").val(),
+                postcode: $("#postcode").val(),
+                phone: $("#phone").val(),
+                phone_mobile: $("#phone_mobile").val()
+            };
+
             if (buttonIndex === 1) {
                 window.customer = data;
                 window.customer.logged = true;
@@ -58,11 +76,67 @@ define(function (require) {
                     trigger: true
                 });
             }
-
-
-
-
-
+        },
+        validate: function (data) {
+            var validity = 0;
+            if (data.firstname.lenght > this.validationparameters.firstname.length.maximum || data.firstname === "" || data.firstname === null) {
+                $('#firstnameError').attr('class', 'mostra');
+                $('#firstnameError').text("Nome obligatorio, lunghezza deve essere inferiore di 32 caratteri");
+                validity++;
+            } else {
+                $('#firstnameError').attr('class', 'nascosto');
+            }
+            if (data.lastname.lenght > this.validationparameters.lastname.length.maximum || data.lastname === "" || data.lastname === null) {
+                $('#lastnameError').attr('class', 'mostra');
+                $('#lastnameError').text("Cognome obligatorio e lunghezza deve essere inferiore di 32 caratteri");
+                validity++;
+            } else {
+                $('#lastnameError').attr('class', 'nascosto');
+            }
+            
+            if (!this.isEmail(data.email)) {
+                $('#emailError').attr('class', 'mostra');
+                $('#emailError').text("Inserisci un email corretta");
+                validity++;
+            } else {
+                $('#emailError').attr('class', 'nascosto');
+            }
+            
+            if (validity === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validationparameters: {
+            firstname: {
+                length: {
+                    maximum: 32,
+                    tooLong: 'Massimo 32 caratteri'
+                }
+            },
+            lastname: {
+                length: {
+                    maximum: 32,
+                    tooLong: 'Massimo 32 caratteri'
+                }
+            },
+            password: {
+                length: {
+                    maximum: 32,
+                    tooLong: 'Massimo 32 caratteri'
+                }
+            },
+            email: {
+                length: {
+                    maximum: 128,
+                    tooLong: 'Massimo 128 caratteri'
+                }
+            }
+        },
+        isEmail: function (email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
         }
     });
 
